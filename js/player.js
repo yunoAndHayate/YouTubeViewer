5b8843666458d4fc7f0dc942b172a70d7ac73bb2
yh <unomaru88101213@gmail.com>
2020-07-11 23:13:00 +0900

    動画URLを指定して、動画を再生できるよう変更。 また、動画が変わったことが分かりやすいよう、モーダルを透明に変更。

--- js/player.js
+++ js/player.js
@@ -20,13 +20,28 @@ function onYouTubeIframeAPIReady() {
             'onStateChange': function (params) { },
         },
     });
-}
-setInterval(function () {
-    document.title = player.getVideoData().title;
-}, 1000);
 
-// 動画の切り替え
-$('.input').on('change', function (value) {
-    // TODO: value がYoutube Video URLの形式だったらその動画に遷移
-    // TODO: そうでなければ単語検索
-});
+    // inputに初期値を設定
+    $(".input").val("https://www.youtube.com/watch?v=" + video_id);
+
+    // 動画の切り替え
+    $('.input').on('change', function () {
+        // value がYoutube Video URLの形式だったらその動画に遷移
+        video_id = $(this).val().match(/(\/embed\/|watch\?v=([^&]+))/);
+        if (video_id && video_id[2]) {
+            console.log('change video to ' + video_id[2] + ' !');
+
+            player.loadVideoById(video_id[2]);
+        }
+        // TODO: そうでなければ単語検索
+        else {
+            console.log('search video!');
+
+        }
+    });
+    // inputへフォーカス時に全選択
+    $('.input').on('focus', function () {
+        $(this).select();
+    });
+
+}
